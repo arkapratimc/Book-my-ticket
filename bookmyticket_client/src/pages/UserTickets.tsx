d98b3173e3fa0fc5a823c55fd53b5ccaee9fcda1
arka1002 <arka.mcu700@gmail.com>
2024-08-18 22:02:45 +0530

    chore: done

--- bookmyticket_client/src/pages/UserTickets.tsx
+++ bookmyticket_client/src/pages/UserTickets.tsx
@@ -2,36 +2,40 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 
 const UserTickets = () => {
+  const { user } = useParams();
 
+  const { isSuccess, data } = useQuery({
+    queryKey: ["bookings by users"],
+    queryFn: () =>
+      fetch("/get-user-bookings", { method: "POST" }).then((res) => res.json()),
+  });
 
-    const { user } = useParams();
+  console.log(isSuccess && data);
+  return (
+    <>
+      <p>Hi {user} !!!!!</p>
+      <p>These are your bookings :- </p>
 
-
-
-    const { isSuccess, data } = useQuery({
-        queryKey: ["bookings by users"],
-        queryFn: () => fetch("/get-user-bookings", { method: "POST" }).then(res => res.json())
-    });
-
-    console.log(isSuccess && data);
-    return <>
-    
-    <p>Hi {user} !!!!!</p>
-    <p>These are your bookings :- </p>
-    
-    {isSuccess && data.map((x, index) => <>
-    
-    <div key={index} style={{ borderBottom: "1px solid black" }}>
-        <p>Film name - {x.film_name}, Hall - {x.hallName}, address - {x.address}</p>
-        <p>Seats booked - {Object.keys(x.seats).map(y => <span>{y} </span>)}</p>
-        <p>Starting time - {x.starting_time}</p>
-    </div>
-    
-    
-    </>)}
-    
+      {isSuccess &&
+        data.map((x, index) => (
+          <>
+            <div key={index} style={{ borderBottom: "1px solid black" }}>
+              <p>
+                Film name - {x.film_name}, Hall - {x.hallName}, address -{" "}
+                {x.address}
+              </p>
+              <p>
+                Seats booked -{" "}
+                {Object.keys(x.seats).map((y, index, arr) => (
+                  <span>{`${y} ${index === arr.length - 1 ? "" : ", "}`} </span>
+                ))}
+              </p>
+              <p>Starting time - {x.starting_time}</p>
+            </div>
+          </>
+        ))}
     </>
+  );
 };
-
 
 export { UserTickets };
